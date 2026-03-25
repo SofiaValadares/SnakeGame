675dc8a2be91c95e84dcf100187026aa67e1698b
SofiaValadares <svc3@cesar.school>
2026-03-25 20:18:27 -0300

    botao portifolio na home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,10 +2,13 @@ import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
 import HomeSelectionBoard from './components/HomeSelectionBoard';
 import './Home.css';
+import {getPortfolioUrl} from "../../config/externalLinks";
 
 interface HomePageProps {}
 
 const HomePage: FC<HomePageProps> = () => {
+  const portfolioUrl = getPortfolioUrl();
+
   return (
     <div className="ds-screen ds-screen--top ds-screen--viewport">
       <div className="ds-panel home-panel">
@@ -22,6 +25,25 @@ const HomePage: FC<HomePageProps> = () => {
           <Link to="/game" className="ds-btn ds-btn--primary">
             Jogar
           </Link>
+          {portfolioUrl ? (
+              <a
+                  href={portfolioUrl}
+                  className="ds-btn ds-btn--ghost"
+                  target="_blank"
+                  rel="noopener noreferrer"
+              >
+                Portfólio
+              </a>
+          ) : (
+              <button
+                  type="button"
+                  className="ds-btn ds-btn--ghost"
+                  disabled
+                  title="Define REACT_APP_PORTFOLIO_URL no .env"
+              >
+                Portfólio (em breve)
+              </button>
+          )}
           <span className="ds-caption ds-blink">▍</span>
         </footer>
       </div>
